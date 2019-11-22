f31d6ff14099a97e4faa7fd5172ca5a676196e11
husainasad <shusain_be16@thapar.edu>
2019-11-22 14:48:49 +0530

    play video as wallpaper

---
+++ video_wallpaper/createframes.py
@@ -0,0 +1,31 @@
+import ctypes
+import time
+import cv2
+import os
+
+def create_frames(path):
+    video = cv2.VideoCapture(path)
+    if video.isOpened() == False:
+        print("Error opening video.")
+        input()
+        return False
+
+    total_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+    fps = video.get(cv2.CAP_PROP_FPS)
+
+    success, image = video.read()
+    for i in range(total_frames):
+        cv2.imwrite(f"frames/frame{i+1}.jpg", image)
+        success, image = video.read()
+        print(f"{i+1}/{total_frames} complete")
+        os.system("cls")
+
+def main():
+    path = input("Enter the path of the video: ")
+    if not os.path.exists(f"{os.getcwd()}/frames"):
+        os.makedirs(f"{os.getcwd()}/frames")
+        
+    create_frames(path)
+
+if __name__ == "__main__":
+    main()
